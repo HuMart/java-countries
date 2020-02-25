4e121d439a96a50481d065cb50bd2caa81345c18
HuMart <manya01@protonmail.com>
2020-02-25 12:05:41 -0500

    creating project

---
+++ src/test/java/com/countries/searchcountries/SearchcountriesApplicationTests.java
@@ -0,0 +1,15 @@
+package com.countries.searchcountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchcountriesApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
